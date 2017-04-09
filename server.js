24c76d43ca2c73f8a4a24bf82492a32b77afb40c
Najm Sheikh <hello@najmsheikh.me>
2017-04-09 01:04:28 -0400

    fix

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const fs = require('fs');
 const express = require('express');
 const bodyParser = require('body-parser');
 const app = express();
-const port = 1989;
+const port = 3000;
 
 app.use(bodyParser.urlencoded({
     extended: false
